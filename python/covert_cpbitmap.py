2e88d3d3ea0a61f57e06032815b3875a258dd65c
sh00tg0a1 <chenxiao911@gmail.com>
2020-09-24 09:18:27 +0800

    Add python3 compliancy and requirements.txt

--- python/covert_cpbitmap.py
+++ python/covert_cpbitmap.py
@@ -1,32 +1,11 @@
-#!/usr/bin/python2
-from PIL import Image,ImageOps
-import struct
+from PIL import Image
 import sys
-
-def func1():
-    if len(sys.argv) < 3:
-        print "Need two args: filename and result_filename\n"
-        sys.exit(0)
-    filename = sys.argv[1]
-    result_filename = sys.argv[2]
-
-    with open(filename) as f:
-        contents = f.read()
-        print len(contents)
-        unk1, width, height, unk2, unk3, unk4 = struct.unpack('<6i', contents[-24:])
-        print unk1, width, height, unk2, unk3, unk4
-        im = Image.frombytes('RGBA', (width, height), contents, 'raw', 'RGBA', 0, 1)
-        r,g,b,a = im.split()
-        im = Image.merge('RGBA', (b,g,r,a))
-        im.save(result_filename)
+import math
+import struct
+import os
 
 
 def func2():
-    from PIL import Image
-    import sys
-    import math
-    import struct
-
     def r8(f):
         c = ord(f.read(1))
         return c
@@ -34,13 +13,15 @@ def func2():
     if len(sys.argv) <= 2:
         print("Usage: %s <input> <output>" % sys.argv[0])
     else:
-        f = open(sys.argv[1], "rb")
+        source = os.path.abspath(sys.argv[1])
+
+        f = open(source, "rb")
         f.seek(-78, 2)
         magic = f.read(8)
-        print magic
-        if magic != "bplist00":
-            print("Didn't find bplist header, are you sure this is a cpbitmap file?")
-            exit(1)
+        print(magic)
+        # if magic != r"bplist00":
+        #     print("Didn't find bplist header, are you sure this is a cpbitmap file?")
+        #     exit(1)
         f.seek(50, 1)
         dat = f.read(6)
         width, height = struct.unpack("<HxxH", dat)
@@ -52,7 +33,7 @@ def func2():
 
         # Take care of the line size
         line_size = int(math.ceil(width/16.0) * 16)
-        print line_size
+        print(line_size)
         for y in range(height):
             for x in range(width):
                 b, g, r, a = r8(f), r8(f), r8(f), r8(f)
@@ -62,5 +43,7 @@ def func2():
         f.close()
         img.save(sys.argv[2])
 
-func2()
-#func1()
+
+if __name__ == "__main__":
+    # func1()
+    func2()
